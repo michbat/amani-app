7aec2ba8de9ccdf0d66a856f556e03948e2ff804
michbat <michael.batn@hotmail.com>
2023-09-22 05:03:46 +0200

    improving

--- database/migrations/2023_09_16_211509_create_bands_table.php
+++ database/migrations/2023_09_16_211509_create_bands_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('bands', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->integer('member')->default(1);
+            $table->integer('member')->default(2);
         });
     }
 
